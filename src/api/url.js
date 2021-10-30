b0f8cfc9d61b5b3f137b0c90a1ba020899e3e1d2
Xen14581 <72184281+Xen14581@users.noreply.github.com>
2021-10-30 13:40:57 +0530

    workaround for no fs

--- src/api/url.js
+++ src/api/url.js
@@ -1,2 +1,3 @@
-export const baseurl = "https://api-autodoc.herokuapp.com";
+// export const baseurl = "https://api-autodoc.herokuapp.com";
 // export const baseurl = "http://localhost:8001";
+export const baseurl = 'https://b093-103-57-252-235.ngrok.io/'
